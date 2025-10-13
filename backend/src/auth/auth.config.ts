9920f344e7343026956a8013f34824da81faa0d4
RodryAlexander <rodrygoleu7@gmail.com>
2025-10-13 10:30:07 -0500

    Adding callbacks to return a JWT to the front for authentication

--- backend/src/auth/auth.config.ts
+++ backend/src/auth/auth.config.ts
@@ -2,6 +2,7 @@ import { ExpressAuthConfig } from "@auth/express"
 import Google from "@auth/express/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@/generated/prisma"
+import jwt from "jsonwebtoken"
 
 
 // we dont load environment var here cause we do it on the global auth 
@@ -11,6 +12,8 @@ const prisma = new PrismaClient()
 
 export const authConfig: ExpressAuthConfig = {
    adapter: PrismaAdapter(prisma),
+   secret: process.env.GOOGLE_CLIENT_SECRET!,
+   session: { strategy: "jwt" },
    providers: [
       Google({
          clientId: process.env.AUTH_GOOGLE_ID!,
@@ -18,8 +21,24 @@ export const authConfig: ExpressAuthConfig = {
       })
    ],
    callbacks: {
-      async session({ session, user }) {
-         session.user.id = user.id
+      async jwt({ token, user }) {        // create a jwt with the user id (from the DB)
+         if (user) { token.id = user.id }
+         return token
+      },
+
+      async session({ session, token }) {
+         // creating custom JWR
+         const accesToken = jwt.sign(
+            {
+               id: token.id,     // the user id (added in the jwt callback)
+               email: token.email,
+               picture: token.picture
+            },
+            process.env.AUTH_GOOGLE_SECRET!,
+         )
+
+         session.user.id = token.id as string
+         session.sessionToken = accesToken
          return session
       }
    }
