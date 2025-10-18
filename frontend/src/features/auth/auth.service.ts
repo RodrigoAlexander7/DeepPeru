a8817689f713492bc5666d14c2355ead71bf9aa1
RodryAlexander <rodrygoleu7@gmail.com>
2025-10-17 20:00:07 -0500

    feat(fix): fiing route for oauth for google

--- frontend/src/features/auth/auth.service.ts
+++ frontend/src/features/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { api } from "@/lib/apis";
 
 export const authService = {
    async GoogleLogin() {
-      window.location.href = `http://localhost:3000/auth/google`
+      window.location.href = `http://localhost:3000/auth/signin`
    },
    getSession: async () => {
       const res = await api.get("/auth/session");
