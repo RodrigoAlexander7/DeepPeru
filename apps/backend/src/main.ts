be94b9979927bc965e9ad046231988641d76561c
alvaro9rqc <alvaro9rqc@gmail.com>
2025-10-28 09:25:12 -0500

    chore: add swagger module to main.ts in nestjs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+const developerDocumentation = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV !== 'production') {
+    developerDocumentation(app);
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('❌ Error during bootstrap:', err);
+  process.exit(1);
+});
