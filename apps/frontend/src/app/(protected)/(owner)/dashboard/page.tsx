12503f8c55b92197441d467de1b6489c27c94334
RodryAlexander <rodrygoleu7@gmail.com>
2025-11-04 01:04:26 -0500

    feat: adjusting redirec from front to back

--- apps/frontend/src/app/(protected)/(owner)/dashboard/page.tsx
+++ apps/frontend/src/app/(protected)/(owner)/dashboard/page.tsx
@@ -1,15 +1,9 @@
 'use client';
-import { useAuthGuard } from '@/hooks/useAuthGuard';
 
 export default function DashboardPage() {
-  const ready = useAuthGuard();
-
-  if (!ready) {
-    return (
-      <div>
-        <p>Loading session...</p>
-      </div>
-    );
-  }
-  return <div>Bienvenido al dashboard</div>;
+  return (
+    <div>
+      <p>Bienvenido</p>
+    </div>
+  );
 }
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Geist, Geist_Mono, PT_Sans } from 'next/font/google';
 import './globals.css';
 
 import { useEffect } from 'react';
-import { useUserStore } from '@/store/userStore';
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
@@ -21,11 +20,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { fetchUser, isLoading } = useUserStore();
-
-  useEffect(() => {
-    fetchUser();
-  }, [fetchUser]);
   return (
     <html lang="en">
       <body className={`${ptSans.className} ${geistMono.variable} antialiased`}>
