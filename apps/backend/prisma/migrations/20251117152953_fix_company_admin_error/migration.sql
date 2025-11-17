223e23a13255bba99f9e04932f98fbc37fe57605
alvaro9rqc <alvaro9rqc@gmail.com>
2025-11-17 10:26:43 -0500

    fix: erase unnecesary id attribute

---
+++ apps/backend/prisma/migrations/20251117152953_fix_company_admin_error/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tourismCompanyId` on the `CompanyAdmin` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CompanyAdmin" DROP COLUMN "tourismCompanyId";
